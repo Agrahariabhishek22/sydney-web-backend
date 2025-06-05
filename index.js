b49b340cb778ba72759f4af3770f5c7c0847237e
Agrahariabhishek22 <balajeet22@gmail.com>
2025-06-05 22:59:30 +0530

    cors

--- index.js
+++ index.js
@@ -8,12 +8,37 @@ const app=express();
 dotenv.config();
 db.connect();
 
-app.use(cors({
-  origin: ['http://localhost:5173','https://sydney-web-frontend.vercel.app'],
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
+const allowedOrigins=['http://localhost:5173','https://sydney-web-frontend.vercel.app']
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+//   credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Access-Control-Allow-Credentials", "true");
+    }
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use("/api/events",eventRoutes);
 app.get('/',(req,res)=>{
